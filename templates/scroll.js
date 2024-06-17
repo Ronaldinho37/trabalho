b612d0a5f351441a52f9dd2a4c3a9c29ec47f469
Ronaldinho37 <ronaldsilvasantos02@gmail.com>
2024-06-17 10:32:41 -0300

    Add files via upload

---
+++ templates/scroll.js
@@ -0,0 +1,31 @@
+ScrollReveal().reveal('#logo');
+
+ScrollReveal().reveal('#logo', 
+{ duration: 1200 ,
+  distance: '150px',
+  origin:'right'});
+
+ScrollReveal().reveal('#text');
+
+  ScrollReveal().reveal('#text', 
+  { duration: 1200 ,
+    distance: '150px',
+    origin:'left'});
+
+ScrollReveal().reveal('#enter-btn');
+
+  ScrollReveal().reveal('#enter-btn', 
+  { duration: 1200 ,
+    distance: '150px',
+    origin:'left'});
+
+ScrollReveal().reveal('#titulo');
+
+  ScrollReveal().reveal('#titulo', 
+  { duration: 1200 ,
+    distance: '150px',
+    origin:'top'});
+    
+  
+  
+
